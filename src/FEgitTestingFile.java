2fa4c7ffac3980b6adf39309ca1a9dddc4ae62fb
Finn-E26 <finn.e_08@outlook.com>
2025-11-03 19:09:54 -0500

    Added some adding functions

---
+++ src/FEgitTestingFile.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FEgitTestingFile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("This is a test file for Git. It works! Copilot wrote this line.");
+
+        // Just a simple test to see if Git is working properly with VS Code. Just trying to learn the basics of Git.
+        // Don't feel like coding anything complicated here.
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("The sum is: " + (a + b)); 
+         
+
+        sc.close();
+    }
+}
